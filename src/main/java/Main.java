98e0c5806cd6d7d00a867df1f459ad2832d18e44
Miguel Monteiro <miguelmonteiroclaveri@gmail.com>
2019-05-13 17:20:10 +0200

    Podemos añadir numeros a nuestra entrada de la operacion matematica

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import visual.InterficieGrafica;
+
+import javax.swing.*;
+
+/**
+ * Creado por: mmonteiro
+ * miguelmonteiroclaveri@gmail.com
+ * github.com/mmonteiroc
+ * Paquete PACKAGE_NAME
+ * Proyecto Calculadora
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        InterficieGrafica frame = new InterficieGrafica();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+}
---
+++ src/main/java/visual/InterficieGrafica.java
@@ -0,0 +1,145 @@
+package visual;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Creado por: mmonteiro
+ * miguelmonteiroclaveri@gmail.com
+ * github.com/mmonteiroc
+ * Paquete visual
+ * Proyecto Calculadora
+ */
+public class InterficieGrafica extends JFrame {
+    private JPanel panel1;
+    private JPanel PanelPrincipal;
+    private JPanel ParteNorte;
+    private JTextField Entrada;
+    private JTextField Salida;
+    private JPanel PanelCentral;
+
+    // KEYPAD
+    private JPanel Keypad;
+    // Numeros
+    private JButton boton0;
+    private JButton boton1;
+    private JButton boton2;
+    private JButton boton3;
+    private JButton boton4;
+    private JButton boton5;
+    private JButton boton6;
+    private JButton boton7;
+    private JButton boton8;
+    private JButton boton9;
+
+    //Botones
+    private JButton resta;
+    private JButton Suma;
+    private JButton botonComa;
+    private JButton botonResultado;
+    private JButton multiplicacion;
+    private JButton dividir;
+
+
+    // Constructor
+    public InterficieGrafica() {
+        // Extablece panel principal
+        this.setContentPane(panel1);
+
+        /*Barra menus*/
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+
+        /*Menu File*/
+        JMenu settings = new JMenu("Ajustes");
+        menuBar.add(settings);
+
+        /*Acciones añadir numeros*/
+
+        boton0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "0");
+
+            }
+        });
+        boton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "1");
+
+            }
+        });
+        boton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "2");
+
+            }
+        });
+        boton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "3");
+
+            }
+        });
+        boton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "4");
+
+            }
+        });
+        boton5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "5");
+
+            }
+        });
+        boton6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "6");
+
+            }
+        });
+        boton7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "7");
+
+            }
+        });
+        boton8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "8");
+
+            }
+        });
+        boton9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Entrada.setText(Entrada.getText() + "9");
+
+            }
+        });
+
+
+    }
+
+
+}
